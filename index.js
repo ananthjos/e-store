495ab5e05fed64d4f2869a7aa5af74f67647ed6a
ananthjos <68706680+ananthjos@users.noreply.github.com>
2021-06-03 17:45:05 +0530

    origin *

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 // connect db
 connectDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:'*'}));
  // origin: "http://pensive-goldwasser-db8ef8.netlify.app"
 app.use("/api/admin", admin);
 app.use("/api/customer", customer);
